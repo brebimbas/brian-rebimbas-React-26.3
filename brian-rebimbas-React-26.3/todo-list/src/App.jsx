d7015414d3c8cfa5411a3875105571f446f0f6d9
Brian Rebimbas <brianrebimbas@Mac.T-mobile.com>
2026-07-29 20:29:59 -0400

    Part 2 Create the Add Todo Handler

--- brian-rebimbas-React-26.3/todo-list/src/App.jsx
+++ brian-rebimbas-React-26.3/todo-list/src/App.jsx
@@ -3,16 +3,24 @@ import TodoList from "./TodoList.jsx";
 import TodoForm from "./TodoForm.jsx";
 import { useState } from "react";
   
-const todos = [
-
-  ];
+const todos = [];
 
 function App() {
   const [todoList, setTodoList] = useState (todos);
+
+  function addTodo(todoTitle){
+    const newTodo ={
+      id: Date.now(),
+      title: todoTitle
+    };
+    setTodoList([...todoList, newTodo])
+
+
+} 
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo}/>
       <TodoList todoList={todoList} />
     </div>
   );
