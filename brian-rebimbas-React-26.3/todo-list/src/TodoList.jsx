71f92950d38141efebe9b9bc434a74fb4f54e8d5
Brian Rebimbas <brianrebimbas@Mac.T-mobile.com>
2026-08-05 22:18:04 -0400

    Add todo Completion Feature

--- brian-rebimbas-React-26.3/todo-list/src/TodoList.jsx
+++ brian-rebimbas-React-26.3/todo-list/src/TodoList.jsx
@@ -1,14 +1,19 @@
  import TodoListItem from "./TodoListItem";
 
- function TodoList({todoList}) {
- const isEmpty = todoList.length === 0;
+ function TodoList({todoList, onCompleteTodo}) {
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted)
+  const isEmpty = filteredTodoList.length === 0;
+ 
 
   return (
     <>
     {isEmpty ? (<p>Add todo above to get started</p>):
       (<ul>
-        {todoList.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} />
+        {filteredTodoList.map((todo) => (
+          <TodoListItem 
+            key={todo.id}
+            todo={todo}
+            onCompleteTodo={onCompleteTodo} />
         ))}
       </ul>)}
     </>
