6c304cb7daadc7d3ba5bda0ccfbba9874fcb1f57
Brian Rebimbas <brianrebimbas@Mac.T-mobile.com>
2026-08-12 23:58:45 -0400

    Fix TodoForm Component

--- brian-rebimbas-React-26.3/todo-list/src/features/TodoForm.jsx
+++ brian-rebimbas-React-26.3/todo-list/src/features/TodoForm.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import TextInputWithLabel from "../shared/TextInputWithLabel";
 import { isValidTodoTitle } from "../utils/todoValidation";
 
 export default function TodoForm() {
+  const [workingTodoTitle, setWorkingTodoTitle] = useState("");
+
+  function handleChange(event) {
+    setWorkingTodoTitle(event.target.value);
+  }
+
   return (
     <form>
-      <TextInputWithLabel elementId="todoTitle" labelText="Todo" />
-      <button type="submit" disabled={!isValidTodoTitle}>
+      <TextInputWithLabel
+        elementId="todoTitle"
+        labelText="Todo"
+        value={workingTodoTitle}
+        onChange={handleChange}
+      />
+      <button type="submit" disabled={!isValidTodoTitle(workingTodoTitle)}>
         Add Todo
       </button>
     </form>
