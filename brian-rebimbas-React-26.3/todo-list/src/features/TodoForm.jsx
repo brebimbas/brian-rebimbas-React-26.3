428963dfaf80754c731df032b3c4b14866a7ca4d
Brian Rebimbas <brianrebimbas@Mac.T-mobile.com>
2026-08-12 22:06:49 -0400

    Create TextInput with Label Component, Refactor TodoForm to use it

--- brian-rebimbas-React-26.3/todo-list/src/features/TodoForm.jsx
+++ brian-rebimbas-React-26.3/todo-list/src/features/TodoForm.jsx
@@ -1,11 +1,12 @@
-function TodoForm () {
-    return (
-        <form>
-            <label htmlFor="todoTitle">Todo</label>
-            <input type="text" id="todoTitle" />
-            <button type="submit" disabled>Add Todo</button>
-        </form>
-    );
-}
+import TextInputWithLabel from "../shared/TextInputWithLabel";
 
-export default TodoForm
+export default function TodoForm() {
+  return (
+    <form>
+      <TextInputWithLabel elementId="todoTitle" labelText="Todo" />
+      <button type="submit" disabled>
+        Add Todo
+      </button>
+    </form>
+  );
+}
