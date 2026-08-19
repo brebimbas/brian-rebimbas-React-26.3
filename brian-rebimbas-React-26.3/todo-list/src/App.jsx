1adab7abe08cd4a405da0639917dea05e32da155
Brian Rebimbas <brebimbas@gmail.com>
2026-08-19 11:04:19 -0400

    Add Login Page

--- brian-rebimbas-React-26.3/todo-list/src/App.jsx
+++ brian-rebimbas-React-26.3/todo-list/src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./shared/Header";
+import Logon from "./features/Logon";
 import TodosPage from "./features/Todos/TodosPage";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+
   return (
     <>
-      <Header />
-      <TodosPage />
+      <Header token={token} onSetToken={setToken} onSetEmail={setEmail} />
+
+      {token ? (
+        <TodosPage token={token} />
+      ) : (
+        <Logon onSetEmail={setEmail} onSetToken={setToken} />
+      )}
     </>
   );
 }
